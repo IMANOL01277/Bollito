e56c0d227d64840ecc369081fd44b7ec388b79ac
IMANOL01277 <imanolmendoza2012@gmail.com>
2025-12-22 02:45:46 -0500

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -441,7 +441,7 @@ $current_page = basename($_SERVER['PHP_SELF'], '.php');
   <div class="sidebar-header">
     <div class="logo-container">
       <div class="logo-icon">
-        <img src="/img/logo_mibollito.png" alt="">
+        <img src="/img/logo_mibollito.png" alt="" height="20px" width="20px">
       </div>
       <h4>Mi Bollito</h4>
       <p>Sistema de Gestión</p>
